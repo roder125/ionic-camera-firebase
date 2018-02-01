ea0014d64b7d3cd82097b17103fc4c28520e6d14
rschuessler <roderick.schuessler8@googlemail.com>
2018-02-01 15:17:10 +0100

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,32 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  captureDataUrl: string;
+
+  constructor(private camera: Camera, public navCtrl: NavController) {
 
   }
+
+  capture(){
+
+    let options = {
+      quality: 50,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      allowEdit: true,
+      saveToPhotoAlbum: false,
+      cameraDirection: 1,
+      sourceType: this.camera.PictureSourceType.CAMERA
+      };
+
+    this.camera.getPicture(options)
+      .then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64:
+      this.captureDataUrl = 'data:image/jpeg;base64,' + imageData;
+     }, (err) => {
+      // Handle error
+     });
+  }
+  
 
 }
